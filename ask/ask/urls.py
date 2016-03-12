57ed75edafc20e131003997db2014c11c3f9ee56
Austry <iliazasimov@gmail.com>
2016-03-12 17:51:05 +0300

    Переделки

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,17 +1,14 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-admin.autodiscover()
+
+import qa.views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^', qa.views.test),
-    url(r'^login/', qa.views.test),
-    url(r'^singup/', qa.views.test),
-    url(r'^question/[0-9]{10}', qa.views.test),
-    url(r'^ask/'. qa.views.test),
-    url(r'^popular/', qa.views.test),
-    url(r'^new/', qa.views.test)
-)
+                       url(r'^login/$', qa.views.test),
+                       url(r'^singup/$', qa.views.test),
+                       url(r'^question/(\d+)', qa.views.test),
+                       url(r'^ask/$', qa.views.error),
+                       url(r'^popular/$', qa.views.test),
+                       url(r'^new/$', qa.views.test)
+                       )
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def test(request, *args, **kwargs):
-	return HttpResponse('OK')
+    return HttpResponse('OK')
 
+def error(request,*args, **kwargs):
+    return HttpResponse('Not ok')
 # Create your views here.
